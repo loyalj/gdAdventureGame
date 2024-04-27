26dce514bfbf195e81203178b8f88f2844af8db5
Loyal Johnson <loyalj@gmail.com>
2024-04-27 17:16:25 -0600

    - Updated the way the navigation mesh works to make it easier to manage - Added a camera to follow the player, so levels can be larger now

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ var windowDimensions
 @export var heightCurve : Curve
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
+@onready var cam: Camera2D = $Camera2D
 
 func _ready():
 	moveTarget = global_position
@@ -14,8 +15,8 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("lmb_click"):
-		moveTarget = event.global_position
-		nav.target_position = event.global_position
+		moveTarget = cam.get_global_mouse_position()
+		nav.target_position = moveTarget
 
 func _process(delta):
 	velocity = global_position.direction_to(nav.get_next_path_position()) * speed
